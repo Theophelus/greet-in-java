8756fce840fe4fef417042719c704ad31d2edad9
Theophelus <aneletom10@gmail.com>
2019-03-28 11:28:10 +0200

    adding src folder with main class

---
+++ src/main/java/Languages.java
@@ -0,0 +1,3 @@
+public enum Languages {
+    Isixhosa, English, IsZulu
+}
---
+++ src/main/java/greet/MainGreet.java
@@ -0,0 +1,11 @@
+package greet;
+//import java.util.Scanner;
+//package greet;
+class MainGreet {
+
+public static void main(String[] args){
+    System.out.println("*** Greeting Console Applications ***");
+//        Scanner myObj = new Scanner(System.in);
+    }
+}
+
