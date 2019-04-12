ca72e6a4e9f8f59c22f5203522181cc84cfafdec
Theophelus <aneletom10@gmail.com>
2019-04-12 18:27:39 +0200

    defining a constructor for connection strings into H2 database

--- src/main/java/counter/Counterjdbc.java
+++ src/main/java/counter/Counterjdbc.java
@@ -2,8 +2,10 @@ package counter;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Objects;
 
-public class Counterjdbc {
+public class Counterjdbc implements GreetCounter {
 
     Connection con;
     //Define a method to for connecton String
@@ -16,15 +18,14 @@ public class Counterjdbc {
             //Register Drivers
             Class.forName("org.h2.Driver");
             //Define a method to create connection strings to the database
-//        return DriverManager.getConnection(URL,user,password);
-//        PreparedStatement addFruitPreparedStatement = con.prepareStatement(INSERT_FRUIT_SQL);
             con = DriverManager.getConnection(URL,user,password);
-
+            final String INSERT_USERS_SQL = "INSERT INTO user(user_name, user_counter) values(?, ?)";
+            //Define a PreparedStatement
+            PreparedStatement addUserPreparedStatement = con.prepareStatement(INSERT_USERS_SQL);
         }catch (Exception e) {
             System.out.println("These test will fail until the fruit table is created: " + e);
         }
-
-
+        Objects.requireNonNull(con).close();
     }
 
     public void addNewUser(String userName) {
