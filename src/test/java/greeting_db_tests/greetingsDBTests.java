bb4a64a2cae7a2647090e6a5e6253483ea10b50d
Theophelus <aneletom10@gmail.com>
2019-04-12 14:58:19 +0200

    adding connection string into the database

--- src/test/java/greeting_db_tests/greetingsDBTests.java
+++ src/test/java/greeting_db_tests/greetingsDBTests.java
@@ -1,17 +1,17 @@
-//package greeting_db_tests;
-//
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//
-//public class greetingsDBTests {
-//
-//    final String DATABASE_URL = "jdbc:h2:./target/greetings_db";
-//
-//    public Connection getconnection() throws Exception {
-//
-//        Connection con = DriverManager.getConnection(DATABASE_URL,"sa","");
-//        return con;
-//    }
-//
-//
-//}
+package greeting_db_tests;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class greetingsDBTests {
+
+    final String DATABASE_URL = "jdbc:h2:./target/greetings_db";
+
+    public Connection getconnection() throws Exception {
+
+        Connection con = DriverManager.getConnection(DATABASE_URL,"sa","");
+        return con;
+    }
+
+
+}
