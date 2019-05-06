deb0b94140fcadc6d66cd580544e70eac2b68530
Theophelus <aneletom10@gmail.com>
2019-05-06 12:34:10 +0200

    adding some tests for commandExtractor class test

--- src/test/java/CommandExtractorTests.java
+++ src/test/java/CommandExtractorTests.java
@@ -1,10 +1,34 @@
+import net.greet.commands.CommandExtractor;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CommandExtractorTests {
 
-//
-//    @Test
-//    public void shouldBeAbleToCheckGreetCommand(){
-//        CommandExtractor commandExtractor = new CommandExtractor("greet");
-//        CommandsProcessor commandsProcessor = new CommandsProcessor();
-//        assertEquals(commandExtractor.getCommand(), commandsProcessor.greets("greet"));
-//    }
+    @Test
+    public void shouldBeAbleToCheckGreetCommand(){
+        CommandExtractor commandExtractor = new CommandExtractor("greet");
+        assertEquals("greet", commandExtractor.getCommand());
+    }
+    @Test
+    public void shouldBeAbleToCheckCommandWithNameAndLang(){
+        CommandExtractor commandExtractor = new CommandExtractor("greet anele english");
+        assertEquals("greet anele english", commandExtractor.getCommand()  + " " + commandExtractor.getUserName() + " " + commandExtractor.getLang());
+    }
+    @Test
+    public void shouldBeAbleToCheckCommandExtractorItHasNameThenReturnTrue(){
+        CommandExtractor commandExtractor = new CommandExtractor("greet anele");
+        assertEquals(true, commandExtractor.hasName());
+    }
+    @Test
+    public void shouldBeAbleToCheckCommandExtractorHasLangThenReturnTrue(){
+        CommandExtractor commandExtractor = new CommandExtractor("greet anele isixhosa");
+        assertEquals(true, commandExtractor.hasLangauge());
+    }
+    @Test
+    public void shouldBeAbleToCheckItGetCommandAndName(){
+        CommandExtractor commandExtractor = new CommandExtractor("greet anele");
+        assertEquals("greet", commandExtractor.getCommand());
+        assertEquals("anele", commandExtractor.getUserName());
+    }
 }
